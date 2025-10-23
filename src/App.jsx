3b07d41ad2d113ec2ece3a494453f05e754ce3fe
Nick.Richardson <90582249+nicjrichDEV@users.noreply.github.com>
2025-10-23 18:05:30 -0400

    Cursor adding some basic page styling.

--- src/App.jsx
+++ src/App.jsx
@@ -6,98 +6,116 @@ import { Testimonial } from "./components/testimonial/Testimonial";
 
 function App() {
   return (
-    <>
-      <section>
-        <h2>Badges</h2>
-        <div className="badges-square">
-          <Badge color="red">Badge</Badge>
-          <Badge color="yellow">Badge</Badge>
-          <Badge color="green">Badge</Badge>
-          <Badge color="blue">Badge</Badge>
-          <Badge color="indigo">Badge</Badge>
-          <Badge color="purple">Badge</Badge>
-          <Badge color="pink">Badge</Badge>
-        </div>
-        <div className="badges-pill">
-          <Badge color="red" shape="pill">
-            Badge
-          </Badge>
-          <Badge color="yellow" shape="pill">
-            Badge
-          </Badge>
-          <Badge color="green" shape="pill">
-            Badge
-          </Badge>
-          <Badge color="blue" shape="pill">
-            Badge
-          </Badge>
-          <Badge color="indigo" shape="pill">
-            Badge
-          </Badge>
-          <Badge color="purple" shape="pill">
-            Badge
-          </Badge>
-          <Badge color="pink" shape="pill">
-            Badge
-          </Badge>
-        </div>
-      </section>
-      <section>
-        <h2>Banners</h2>
-        <div className="banner-multi">
-          <Banner
-            title="Update available"
-            description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur, ipsum similique veniam."
-          />
-          <Banner
-            title="Congratulations!"
-            description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur, ipsum similique veniam."
-            status="success"
-          />
-          <Banner
-            title="Attention!"
-            description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur, ipsum similique veniam."
-            status="warning"
-          />
-          <Banner
-            title="There is a problem with your application!"
-            description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur, ipsum similique veniam."
-            status="error"
-          />
-        </div>
-        <div className="banner-multi">
-          <Banner title="Update available" />
-          <Banner title="Congratulations!" status="success" />
-          <Banner title="Attention!" status="warning" />
-          <Banner
-            title="There is a problem with your application!"
-            status="error"
-          />
-        </div>
-      </section>
-      <section>
-        <h2>Card</h2>
-        <div className="cards">
-          <Card
-            title="Easy Deployment"
-            description="Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis."
-          />
-          <Card
-            title="Easy Deployment"
-            description="Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis."
-            hovered={true}
-          />
-        </div>
-      </section>
-      <section>
-        <h2>Testimonial</h2>
-        <Testimonial
-          quote="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed urna nulla vitae laoreet augue. Amet feugiat est integer dolor auctor adipiscing nunc urna, sit."
-          name="May Andersons"
-          business="Workcation, CTO"
-        />
-      </section>
-    </>
+    <div className="app">
+      <header className="header">
+        <h1>Scrimba Components</h1>
+        <p>A collection of reusable UI components</p>
+      </header>
+
+      <main className="main">
+        <section className="section">
+          <h2>Badges</h2>
+          <div className="component-group">
+            <div className="badges-square">
+              <Badge color="red">Badge</Badge>
+              <Badge color="yellow">Badge</Badge>
+              <Badge color="green">Badge</Badge>
+              <Badge color="blue">Badge</Badge>
+              <Badge color="indigo">Badge</Badge>
+              <Badge color="purple">Badge</Badge>
+              <Badge color="pink">Badge</Badge>
+            </div>
+            <div className="badges-pill">
+              <Badge color="red" shape="pill">
+                Badge
+              </Badge>
+              <Badge color="yellow" shape="pill">
+                Badge
+              </Badge>
+              <Badge color="green" shape="pill">
+                Badge
+              </Badge>
+              <Badge color="blue" shape="pill">
+                Badge
+              </Badge>
+              <Badge color="indigo" shape="pill">
+                Badge
+              </Badge>
+              <Badge color="purple" shape="pill">
+                Badge
+              </Badge>
+              <Badge color="pink" shape="pill">
+                Badge
+              </Badge>
+            </div>
+          </div>
+        </section>
+
+        <section className="section">
+          <h2>Banners</h2>
+          <div className="component-group">
+            <div className="banner-multi">
+              <Banner
+                title="Update available"
+                description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur, ipsum similique veniam."
+              />
+              <Banner
+                title="Congratulations!"
+                description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur, ipsum similique veniam."
+                status="success"
+              />
+              <Banner
+                title="Attention!"
+                description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur, ipsum similique veniam."
+                status="warning"
+              />
+              <Banner
+                title="There is a problem with your application!"
+                description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur, ipsum similique veniam."
+                status="error"
+              />
+            </div>
+            <div className="banner-multi">
+              <Banner title="Update available" />
+              <Banner title="Congratulations!" status="success" />
+              <Banner title="Attention!" status="warning" />
+              <Banner
+                title="There is a problem with your application!"
+                status="error"
+              />
+            </div>
+          </div>
+        </section>
+
+        <section className="section">
+          <h2>Cards</h2>
+          <div className="component-group">
+            <div className="cards">
+              <Card
+                title="Easy Deployment"
+                description="Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis."
+              />
+              <Card
+                title="Easy Deployment"
+                description="Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis."
+                hovered={true}
+              />
+            </div>
+          </div>
+        </section>
+
+        <section className="section">
+          <h2>Testimonial</h2>
+          <div className="component-group">
+            <Testimonial
+              quote="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed urna nulla vitae laoreet augue. Amet feugiat est integer dolor auctor adipiscing nunc urna, sit."
+              name="May Andersons"
+              business="Workcation, CTO"
+            />
+          </div>
+        </section>
+      </main>
+    </div>
   );
 }
 
